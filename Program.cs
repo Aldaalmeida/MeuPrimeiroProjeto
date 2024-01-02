c336a54900654d581ff94ed357947cfc84c75810
Aldaalmeida <aldaalmeida784@gmail.com>
2024-01-02 16:23:41 -0300

    Remoção de Espaços

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var tercaFeira = "Terça-Feira: Estudar Lógica de Programação com C# Udemy";
 var quartaFeira = "Quarta-Feira: Estudar conteúdos da Faculdade";
 var quintaFeira = "Quinta-Feira: Dar continuidade ao curso de Lógica da DIO";
 var sextaFeira = "Sexta-Feira: Dar continuidade ao curso de Lógica Udemy";
+
  const string  messageCronogramaDaProximaSemana = "Na Semana que vêm este será o meu cronograma de estudos: ";
 
 Console.WriteLine(messageCronogramaDaProximaSemana +
