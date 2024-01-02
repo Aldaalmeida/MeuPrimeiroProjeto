3895067347856e6e4cad0fd471098ea7103c906e
aldaf <aldaf@ALDA-ALMEIDA>
2024-01-02 16:04:43 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿var segundaFeira = "Segunda-Feira: Estudar Lógica de Programação DIO";
+var tercaFeira = "Terça-Feira: Estudar Lógica de Programação com C# Udemy";
+var quartaFeira = "Quarta-Feira: Estudar conteúdos da Faculdade";
+var quintaFeira = "Quinta-Feira: Dar continuidade ao curso de Lógica da DIO";
+var sextaFeira = "Sexta-Feira: Dar continuidade ao curso de Lógica Udemy";
+ const string  messageCronogramaDaProximaSemana = "Na Semana que vêm este será o meu cronograma de estudos: ";
+
+
+
+Console.WriteLine(messageCronogramaDaProximaSemana +
+segundaFeira + " - " +
+tercaFeira + " - " +
+quartaFeira + " - " +
+quintaFeira + " - " +
+sextaFeira);
